c8da85971205f6e4c94e306fefd5dc63c9e4a304
s-014-14 <s-014-14@gumiforest.ae.krakow.pl>
2022-05-30 14:25:10 +0200

    payu

--- src/test/java/pl/jpraz/sales/OrderingTest.java
+++ src/test/java/pl/jpraz/sales/OrderingTest.java
@@ -24,18 +24,16 @@ public class OrderingTest {
 
     @Test
     void itAllowsToOrderCollectedProducts() {
+        //Given // Arrange
         String productId = thereIsExampleProduct();
         Sales sales = thereIsSalesModule();
         String customerId = thereIsCustomer();
         sales.addToCart(customerId, productId);
         Offer seenOffer = sales.getCurrentOffer(customerId);
-        //when
+        //when //act
+        PaymentData payment = sales.acceptOffer(customerId, seenOffer, exampleCustomerData());
 
-        PaymentData payment = sales.acceptOffer(
-                customerId,
-                seenOffer,
-                getExampleClientData());
-        //payemntUrl
+        //then // assert
         String reservationId = payment.getReservationId();
 
         assertNotNull(payment.getUrl);
@@ -63,7 +61,7 @@ public class OrderingTest {
             sales.acceptOffer(
                     customerId,
                     newOffer,
-                    getExampleClientData());
+                    exampleCustomerData());
         });
     }
 
@@ -89,7 +87,7 @@ public class OrderingTest {
         return "Kuba";
     }
 
-    private ClientData getExampleClientData() {
+    private ClientData exampleCustomerData() {
         return ClientData.builder()
                 .firstname("John")
                 .lastname("Doe")
